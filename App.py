cdcdb270fd8aec298577929c64a1215a66895540
Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2025-03-10 21:40:13 +0530

    Create Product model

--- App.py
+++ App.py
@@ -5,4 +5,39 @@ import os
 
 # Init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize db
+db = SQLAlchemy(app)
+# Initialize ma
+ma = Marshmallow(app)
+
+# Product class/model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+# Product schema
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"message": "Hello World"})
+
+# Run server
+if __name__=='__main__':
+    app.run(debug=True)
 
